4005ac0646d3c46c0b5380e28e4807601f699fc9
Gil González <fragilgon@gmail.com>
2024-08-17 14:27:11 +0200

    started

---
+++ tailwind.config.mjs
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors:{
+			black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      green: colors.green,
+      red: colors.red,
+      transparent: 'transparent',
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      'yellow-translucent': 'rgba(234, 179, 0, 0.30)',
+			'camouflage': {
+        '50': '#fff9e4',
+        '100': '#fff4c5',
+        '200': '#ffed92',
+        '300': '#ffe753',
+        '400': '#fce41f',
+        '500': '#e2cd00',
+        '600': '#b5aa00',
+        '700': '#898102',
+        '800': '#6c6708',
+        '900': '#5b580c',
+        '950': '#2e2e00',
+   	},
+		},
+		extend: {
+      boxShadow: {
+        'illuminate': '0 0 30px 10px rgba(234, 179, 8, 0.45)'
+      },
+      animation: {
+        'shake': 'shake 0.5s infinite', // Duración de la animación y repetición infinita
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },     // Posición inicial y final
+          '10%, 30%, 50%, 70%, 90%': { transform: 'translateX(-2px)' }, // Movimiento hacia la izquierda
+          '20%, 40%, 60%, 80%': { transform: 'translateX(2px)' },  // Movimiento hacia la derecha
+        },
+      },
+    },
+	},
+	plugins: [],
+}
